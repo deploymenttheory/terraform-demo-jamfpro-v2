2def52af20ca9c20a3a4b51568ff1abce90075eb
ShocOne <62835948+ShocOne@users.noreply.github.com>
2025-03-07 12:26:11 +0000

    feat: added static user group 2

--- workload/terraform/jamfpro/usergroups.tf
+++ workload/terraform/jamfpro/usergroups.tf
@@ -7,3 +7,13 @@ resource "jamfpro_user_group" "static_user_group_001" {
 
   assigned_user_ids = [2,3]
 }
+
+resource "jamfpro_user_group" "static_user_group_002" {
+  name                = "tfc-user-group-static-02"
+  is_smart            = false
+  is_notify_on_change = false
+
+  site_id = 5
+
+  assigned_user_ids = [2,3]
+}
